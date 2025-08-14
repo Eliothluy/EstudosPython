3c97d15b33441d744e6573824c458e5c96915ab2
Elioth <eliothluy@gmail.com>
2025-08-14 20:36:31 -0300

    primeiro arquivo

---
+++ pep8.py
@@ -0,0 +1,12 @@
+'''
+Pep8 - Python Enhancement Proposal
+São propostas de melhorias para a linguagen Python
+The Zen of Python
+import this
+
+A ideia da Pep8 é que possamos escrever código python de forma pythonica
+
+[1] - Utilize Camel Case para nomes de classe;
+[2] - 
+'''
+print("Hello Python")
