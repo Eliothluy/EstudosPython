f249091749678f4edade70095405a82cc2d1207d
Elioth <eliothluy@gmail.com>
2025-08-15 20:30:03 -0300

    primeiros exercicios

---
+++ exercicios/exercicio001.py
@@ -0,0 +1,7 @@
+'''
+Faça um programa que leia um número inteiro e imprima-o.
+
+'''
+
+numero = input('Digite um número: ')
+print(f'O número digitado foi: {numero}')
---
+++ exercicios/exercicio003.py
@@ -0,0 +1,8 @@
+'''
+3. Faça um programa que recebe três valores e apresente a soma dos quadrados dos valores lidos.
+'''
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+numero3 = int(input('Digite o terceiro número: '))
+somaquadrado = (numero1**2) + (numero2**2) + (numero3**2)
+print(f'A soma dos 3 valores ao quadrado é:{somaquadrado}')
