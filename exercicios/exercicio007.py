a226ce5f981f3ca61eb218f70ff07ef92e85a435
Elioth <eliothluy@gmail.com>
2025-08-28 20:46:20 -0300

    atualizando

---
+++ exercicios/exercicio007.py
@@ -0,0 +1,10 @@
+'''
+Faça um programa que determine e mostre os cinco primeiros múltiplos de 3, considerando números
+maiores que 0.
+'''
+
+for numero in range(1, 16):
+    if numero % 3 == 0:
+        print (numero)
+
+
