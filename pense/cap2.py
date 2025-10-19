f23bbc518bc5c343ccd2f53797641998894ff004
Elioth <eliothluy@gmail.com>
2025-10-18 21:14:58 -0300

    capitulo 2 pensem em python terminado

---
+++ pense/cap2.py
@@ -0,0 +1,36 @@
+'''
+Parte 1.
+#raio em cm
+radius = 5
+#volume em cm cubicos
+volume = (3/4)*math.pi*radius**3
+
+print(f'O volume da esfera de raio {radius} é {volume}')
+'''
+import math
+
+'''
+parte 2
+Trigonometria
+
+x = 42
+
+cosDeX= math.cos(x)
+print(f'O cos de {x} vale {cosDeX}')
+senDeX= math.sin(x)
+print(f'O sen de {x} vale {senDeX}')
+
+somaDosQuadrados = cosDeX*cosDeX + senDeX*senDeX
+print(f'A soma dos quadrados de sen e cos x vale {somaDosQuadrados}')
+'''
+
+
+'''
+Parte 3
+log natural
+'''
+print(math.e**2)
+print(math.pow(math.e,2 ))
+print(math.exp(2))
+
+
