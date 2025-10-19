2f67ecd365decaa2aa79e449ec61223ae952d91c
eliothluy <eliothluy@gmail.com>
2025-10-18 23:15:51 -0300

    cap1

---
+++ pense/cap1.py
@@ -0,0 +1,8 @@
+# Calcula quantos segundos existem em 42 minutos e 42 segundos
+
+minutos = 42
+segundos = 42
+
+total_segundos = minutos * 60 + segundos
+
+print("Total de segundos em 42 minutos e 42 segundos:", total_segundos)
