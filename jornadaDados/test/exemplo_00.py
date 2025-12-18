665ad083a8ebb3641bdf729d5989db37af12ddf5
eliothluy <eliothluy@gmail.com>
2025-12-18 19:33:14 -0300

    reiniciando jornada de dados

--- jornadaDados/test/exemplo_00.py
+++
@@ -1,5 +0,0 @@
-#Criar um programa onde o usuário digite dois valores e apareça a soma
-valor1 = int(input("digite o primeiro valor: "))
-valor2 = int(input("digite o segundo valor: "))
-soma = valor1 + valor2
-print("A soma dos valores é: ", soma)
